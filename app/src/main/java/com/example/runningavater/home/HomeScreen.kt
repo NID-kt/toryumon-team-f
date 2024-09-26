fe93a9c0d87a550f9c39bb163795ff1efb5196cd
kdix-23-071 <2312110071r@kindai.ac.jp>
2024-09-26 22:39:46 +0900

    トップバーの表示

--- app/src/main/java/com/example/runningavater/home/HomeScreen.kt
+++ app/src/main/java/com/example/runningavater/home/HomeScreen.kt
@@ -1,8 +1,63 @@
 package com.example.runningavater.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.runningavater.ui.theme.NuclearMango
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Bear3D(assetFileLocation = "fatBear.glb")
+
+    // 現在の日付と曜日を保持するState
+    val currentDate = remember { mutableStateOf(getCurrentDate()) }
+
+    // 日時を更新するためのLaunchedEffect
+    LaunchedEffect(Unit) {
+        while (true) {
+            currentDate.value = getCurrentDate()
+            kotlinx.coroutines.delay(60000L) // 1分ごとに更新
+        }
+    }
+    Scaffold(
+        topBar = {//ヘッダーの表示
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = NuclearMango,
+                    titleContentColor = MaterialTheme.colorScheme.background,
+                ),
+                title = {
+                    // 日付と曜日を表示
+                    Text(text = currentDate.value)
+                }
+            )
+
+        },
+    ) {
+        Box(Modifier.padding(it)) {
+            Bear3D(assetFileLocation = "fatBear.glb")
+        }
+    }
+
+}
+
+// 現在の日付と曜日を取得する関数
+//トップバーで使ってます
+fun getCurrentDate(): String {
+    // 日付と曜日をフォーマットする（例：09/11 (Wed)）
+    val dateFormat = SimpleDateFormat("MM/dd (E)", Locale.getDefault())
+    return dateFormat.format(Date())
 }
