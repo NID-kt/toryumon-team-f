0e755ae6be4e08a7ef84dfd21f0e9ffb29be9a81
kdix-23-071 <2312110071r@kindai.ac.jp>
2024-12-05 22:01:07 +0900

    ホーム画面にお花を表示しました

--- app/src/main/java/com/example/runningavater/home/HomeScreen.kt
+++ app/src/main/java/com/example/runningavater/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.runningavater.home
 
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +21,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.runningavater.ui.theme.NuclearMango
@@ -63,16 +68,28 @@ fun HomeScreen() {
                         .offset(y = 15.dp),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
+                SetImage(
+                    fileName = "flowers_big.PNG",
+                    modifier = Modifier.size(120.dp),
+                )
+
+                // 歩数の数字の部分
                 Text(
                     text = "35",
                     fontSize = 96.sp,
                     color = NuclearMango,
                 )
+                // 調整のためのスペース挿入
                 Spacer(Modifier.width(11.dp))
+                // ”歩”の文字
                 Text(
                     text = "歩",
                     fontSize = 32.sp,
                     color = NuclearMango,
+                )
+                SetImage(
+                    fileName = "flowers_right.PNG",
+                    modifier = Modifier.size(120.dp),
                 )
             }
         }
@@ -85,4 +102,22 @@ fun getCurrentDate(): String {
     // 日付と曜日をフォーマットする（例：09/11 (Wed)）
     val dateFormat = SimpleDateFormat("MM/dd (E)", Locale.getDefault())
     return dateFormat.format(Date())
+}
+
+@Composable
+fun SetImage(
+    fileName: String,
+    modifier: Modifier = Modifier,
+) {
+    // assets フォルダから画像をロード
+    val context = LocalContext.current
+    val assetManager = context.assets
+    val bitmap = BitmapFactory.decodeStream(assetManager.open(fileName))
+
+    // 画像を表示
+    Image(
+        bitmap = bitmap.asImageBitmap(),
+        contentDescription = null,
+        modifier = modifier,
+    )
 }
