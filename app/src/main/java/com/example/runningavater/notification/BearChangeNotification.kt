e726835e1cf25b36e5bc6614f452081a9946a898
Konayashi <takumik0321@icloud.com>
2025-02-13 19:18:47 +0900

    くまが太ったときの通知の内容と、定期的な通知のチャンネルと通知の内容を作成した。

--- app/src/main/java/com/example/runningavater/notification/BearChangeNotification.kt
+++ app/src/main/java/com/example/runningavater/notification/BearChangeNotification.kt
@@ -11,12 +11,35 @@ import com.example.runningavater.R
 fun notifySlim(
     context: Context,
     bearname: String,
-)  {
+) {
     var builder =
         NotificationCompat.Builder(context, "bear_change")
             .setSmallIcon(R.drawable.notification_icon)
             .setContentTitle("${bearname}からの報告")
             .setContentText("${bearname}が呼んでます！")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+    with(NotificationManagerCompat.from(context)) {
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return@with
+        }
+        // notificationId is a unique int for each notification that you must define.
+        notify(1, builder.build())
+    }
+}
+
+fun notyfyFat(
+    context: Context,
+    bearname: String,
+) {
+    var builder =
+        NotificationCompat.Builder(context, "bear_change")
+            .setSmallIcon(R.drawable.notification_icon)
+            .setContentTitle("${bearname}からの報告")
+            .setContentText("${bearname}が呼んでます....")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     with(NotificationManagerCompat.from(context)) {
         if (ActivityCompat.checkSelfPermission(
--- app/src/main/java/com/example/runningavater/notification/NotificationChannel.kt
+++ app/src/main/java/com/example/runningavater/notification/NotificationChannel.kt
@@ -9,15 +9,26 @@ import androidx.core.content.ContextCompat.getSystemService
 fun createNotificationChannels(
     context: Context,
     bearname: String,
-)  {
+) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationManager = getSystemService(context, NotificationManager::class.java) as NotificationManager
+
         // クマの名前の変化のチャンネル
-        val name = "${bearname}の変化"
-        val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val mChannel = NotificationChannel("bear_change", name, importance)
+        val changeChannel =
+            NotificationChannel(
+                "bear_change",
+                "${bearname}の変化",
+                NotificationManager.IMPORTANCE_HIGH,
+            )
+        val regularlyChannel =
+            NotificationChannel(
+                "regularly_notification",
+                "定期的な通知",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
         // Register the channel with the system. You can't change the importance
         // or other notification behaviors after this.
-        val notificationManager = getSystemService(context, NotificationManager::class.java) as NotificationManager
-        notificationManager.createNotificationChannel(mChannel)
+        notificationManager.createNotificationChannel(changeChannel)
+        notificationManager.createNotificationChannel(regularlyChannel)
     }
 }
