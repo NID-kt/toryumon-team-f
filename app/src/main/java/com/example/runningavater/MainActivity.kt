8bf635cc5e76b33e99f8f3fec267bd9ba11b7d3b
Konayashi <takumik0321@icloud.com>
2025-03-07 08:43:24 +0900

    conflictを解消した

--- app/src/main/java/com/example/runningavater/MainActivity.kt
+++ app/src/main/java/com/example/runningavater/MainActivity.kt
@@ -1,4 +1,7 @@
 package com.example.runningavater
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,14 +10,30 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startForegroundService
+import androidx.fragment.app.FragmentActivity
 import com.example.runningavater.notification.createNotificationChannels
 import com.example.runningavater.ui.theme.RunningAvaterTheme
+import android.Manifest
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACTIVITY_RECOGNITION),
+                1001,
+            )
+        }
         // /今は一旦ここに置いているがクマの名前を登録してから実行する
         createNotificationChannels(context = this, bearname = "権左衛門")
+        startStepCounterService()
         setContent {
             RunningAvaterTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,6 +45,16 @@ class MainActivity : ComponentActivity() {
                     MyAppNavHost()
                 }
             }
+        }
+    }
+
+
+    private fun startStepCounterService() {
+        val intent = Intent(this, StepCounterService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
         }
     }
 }
