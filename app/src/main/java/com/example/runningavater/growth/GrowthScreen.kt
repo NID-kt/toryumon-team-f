463f848f1b87e182684f4a661510d78d01e8f1b3
Konayashi <takumik0321@icloud.com>
2025-05-06 21:44:29 +0900

    グラフを整えた

--- app/src/main/java/com/example/runningavater/growth/GrowthScreen.kt
+++ app/src/main/java/com/example/runningavater/growth/GrowthScreen.kt
@@ -12,14 +12,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.example.runningavater.MainApplication
+import com.example.runningavater.home.toEpochMillis
 import com.example.runningavater.ui.theme.GranulatedSugar
 import com.example.runningavater.ui.theme.NuclearMango
 import com.example.runningavater.ui.theme.SungYellow
@@ -27,6 +33,13 @@ import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import dataStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import targetSteps
+import java.time.LocalDateTime
 
 @Composable
 fun GrowthScreen() {
@@ -38,7 +51,7 @@ fun GrowthScreen() {
                 .fillMaxSize(),
     ) {
         Text(
-            text = "成長記録",
+            text = "今日の達成率",
             fontSize = 30.sp,
             modifier =
                 Modifier
@@ -66,10 +79,41 @@ fun GrowthScreen() {
 
 @Composable
 fun PieChart(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val stepcount = remember { mutableStateOf<Int?>(null) }
+    val targetstepscount = remember { mutableStateOf<Int?>(null) }
+    LaunchedEffect(key1 = Unit) {
+        val now = LocalDateTime.now() // 2025/02/23 23:52:10.123
+
+        val todayStart =
+            now
+                .withHour(0) // 2025/02/23 00:52:10.123
+                .withMinute(0) // 2025/02/23 00:00:10.123
+                .withSecond(0) // 2025/02/23 00:00:00.123
+                .withNano(0) // 2025/02/23 00:00:00.000000
+
+        val todayEnd =
+            now
+                .withHour(23) // 2025/02/23 00:52:10.123
+                .withMinute(59) // 2025/02/23 00:00:10.123
+                .withSecond(59) // 2025/02/23 00:00:00.123
+                .withNano(999999999) // 2025/02/23 00:00:00.000000
+        launch(Dispatchers.IO) {
+            val app = context.applicationContext as MainApplication
+            stepcount.value = app.db.stepDateDao().getTotalWalk(todayStart.toEpochMillis(), todayEnd.toEpochMillis())
+            targetstepscount.value = app.dataStore.data.first()[
+                targetSteps
+            ]
+
+        }
+    }
+    if (targetstepscount.value == null) {
+        return
+    }
     val pieEntryList =
         listOf(
-            PieEntry(80f, ""),
-            PieEntry(20f, ""),
+            PieEntry((stepcount.value?.toFloat()?: 0f)/targetstepscount.value!!*100, "","達成率"),
+            PieEntry(100 - (stepcount.value?.toFloat()?: 0f)/targetstepscount.value!!*100, "","未達成率"),
         )
 
     val pieDataSet =
@@ -84,7 +128,15 @@ fun PieChart(modifier: Modifier = Modifier) {
                 setCenterTextSize(18f)
                 setCenterTextColor(NuclearMango.toArgb())
                 setEntryLabelTextSize(11f)
-                data = PieData(pieDataSet).apply { setValueTextSize(20f) }
+                data = PieData(pieDataSet).apply { setValueTextSize(20f)
+                setValueFormatter(object : ValueFormatter() {
+                    override fun getPieLabel(value: Float, pieEntry: PieEntry?): String {
+                        if (pieEntry?.data == "達成率"){
+                            return "${value}%"
+                        }
+                        return ""
+                    }
+                })}
                 // アニメーションを指定
                 animateXY(1000, 1000)
                 // 判例を非表示
